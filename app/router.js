7800cbe652cab9187b45ff9f33d298b50009162c
Lenora Porter <lporter630@gmail.com>
2019-09-16 20:29:22 -0400

    Add routes for orgs, org, repos, repo, contributor and issues

--- app/router.js
+++ app/router.js
@@ -7,6 +7,14 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
+  this.route('orgs', {}); // /orgs
+  this.route('org', {path: 'org/:id'}, function() { //org/emberjs
+    this.route('repos', {});
+    this.route('repo', {path: ':id'}, function() {
+      this.route('contributors');
+      this.route('issues');
+    }); // org/jquery/jquery-ui
+  });
 });
 
 export default Router;
