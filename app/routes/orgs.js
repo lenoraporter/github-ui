2122182bab8f4a16d44ab6a9bba925f8b3732fab
Lenora Porter <lporter630@gmail.com>
2019-09-17 20:36:08 -0400

    Add services, actions, and simple object properties

--- app/routes/orgs.js
+++ app/routes/orgs.js
@@ -1,6 +1,16 @@
 import Route from '@ember/routing/route';
+import Ember from 'ember';
 
 export default Route.extend({
+
+  favorites: Ember.inject.service(),
+
+  actions: {
+    favoriteClicked(org) {
+      this.get('favorites').favoriteItem(org);
+    }
+  },
+
   model() {
     return [{
       id: "emberjs"
---
+++ app/services/favorites.js
@@ -0,0 +1,14 @@
+import Service from '@ember/service';
+
+export default Service.extend({
+  items: [],
+
+  favoriteItem(item) {
+    this.get('items').addObject(item);
+    console.log(
+      this.get('items')
+        .map(x => x.id)
+        .join(', ')
+      );
+  }
+});
